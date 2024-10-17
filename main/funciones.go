91050f4c7280fefe674e8f6352541fb5bbae9ee2
atilio70 <atiliostefani@gmail.com>
2024-10-16 23:29:08 -0300

    Media parte de funciones

--- main/funciones.go
+++ main/funciones.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
-func suma(a, b int) int {
-
-	return a + b
-}
-
-func presentaResultado(a, b int) {
+func presentaSuma(suma func(int, int) int, a, b int) {
 	fmt.Println("La suma de ", a, " y ", b, " es:", suma(a, b))
 }
 
+func presentaResta(resta func(int, int) int, a, b int) {
+	fmt.Println("La resta de ", a, " y ", b, " es:", resta(a, b))
+}
+
+func presentaResultado(operacion string, a int, b int) {
+	suma := func(a, b int) int {
+		return a + b
+	}
+	resta := func(a, b int) int {
+		return a - b
+	}
+
+	resultado := 0
+
+	if operacion == "sumar" {
+		resultado = suma(a, b)
+	} else if operacion == "restar" {
+		resultado = resta(a, b)
+	}
+	fmt.Println("La ", operacion, " de ", a, " y ", b, " es:", resultado)
+
+}
+
 func main() {
-	presentaResultado(5, 8)
+
+	presentaResultado(suma, 5, 8)
 }
